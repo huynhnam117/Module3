8556666f58b65fe0cd5b29e5f3a1897bbd3c0208
huynhnam117 <108474254+huynhnam117@users.noreply.github.com>
2022-10-19 07:37:02 +0700

    up bai

--- BaiTap3/Quan_li_ban_hang.sql
+++ BaiTap3/Quan_li_ban_hang.sql
@@ -64,13 +64,16 @@ values
 -- 	Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
 select oID,oDate,oTotalPrice
 from `order`;
+
 -- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
 select Customer.cName, Produt.pName from customer
 inner join `order` on customer.cId = `order`.cId
 inner join orderDetail on `order`.oId = orderDetail.oId
 inner join produt on orderDetail.pId = produt.pId;
+
 -- Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
 select * from customer where customer.cID not in (select cId from `order`);
+
 -- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn.
 -- Giá bán của từng loại được tính = odQTY*pPrice)
 select `order`.oId,`order`.oDate,SUM(OrderDetail.odQTY * Produt.pPrice) as oTotalPrice
